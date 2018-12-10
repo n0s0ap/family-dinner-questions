8e3b9c6043ae3068918657a4d7a55708b7c69366
Ben Lew <mail@benlew.com>
2018-12-09 22:29:05 -0700

    Fixing notification to fire every day.

--- FDQ_v1/Family Dinner Questions/theNotifier.swift
+++ FDQ_v1/Family Dinner Questions/theNotifier.swift
@@ -15,6 +15,26 @@ func notifyAtDinnerTime() {
     let defaults = UserDefaults.standard
     let theTimeFromSettings:Date! = defaults.value(forKey: "notifyMeTime") as? Date
     print("dinner time is set at \(String(describing: theTimeFromSettings))")
+   
+    let timecomponents = Calendar.current.dateComponents([.hour, .minute], from: theTimeFromSettings)
+    
+    let today = Date()
+    let calendar = NSCalendar.current
+    let components = calendar.dateComponents([.day, .month, .year], from: today)
+    
+    var dateComp:DateComponents = DateComponents()
+    dateComp.day = components.day
+    dateComp.month = components.month
+    dateComp.year = components.year
+    dateComp.hour = timecomponents.hour
+    dateComp.minute = timecomponents.minute
+    dateComp.second = 00
+    let date = calendar.date(from: dateComp)
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd MM yyyy hh:mm:ss"
+    let fireDate = dateFormatter.string(from: date!)
+    print("fireDate: \(fireDate)")
     
     let questionOfftheTop = theSavedQuestions[0]
     
@@ -23,8 +43,10 @@ func notifyAtDinnerTime() {
     localNotification.alertAction = "see more questions"
     localNotification.alertBody = "Time for a dinner question: \(questionOfftheTop)"
     localNotification.soundName = UILocalNotificationDefaultSoundName
-    localNotification.fireDate = theTimeFromSettings
-    localNotification.repeatInterval = NSCalendar.Unit.day
+    localNotification.fireDate = date
+    localNotification.repeatInterval = .day
+    //localNotification.fireDate = theTimeFromSettings
+   // localNotification.repeatInterval = NSCalendar.Unit.day
     UIApplication.shared.scheduleLocalNotification(localNotification)
     let theScheduledNotifications = UIApplication.shared.scheduledLocalNotifications
     print("the schedule is: \(String(describing: theScheduledNotifications))")
